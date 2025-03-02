de41df90146897cb616ff9bc37e615a5e33a6094
Richard <1641070557@qq.com>
2025-03-02 10:27:55 +0800

    Optimize the cart service module

--- app/cart/biz/service/add_item.go
+++ app/cart/biz/service/add_item.go
@@ -1,17 +1,3 @@
-// Copyright 2024 CloudWeGo Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package service
 
 import (
@@ -28,14 +14,13 @@ import (
 
 type AddItemService struct {
 	ctx context.Context
-} // NewAddItemService new AddItemService
+}
+
 func NewAddItemService(ctx context.Context) *AddItemService {
 	return &AddItemService{ctx: ctx}
 }
 
-// Run create note info
 func (s *AddItemService) Run(req *cart.AddItemReq) (resp *cart.AddItemResp, err error) {
-	// Finish your business logic.
 	getProduct, err := rpc.ProductClient.GetProduct(s.ctx, &product.GetProductReq{Id: req.Item.GetProductId()})
 	if err != nil {
 		return nil, err
